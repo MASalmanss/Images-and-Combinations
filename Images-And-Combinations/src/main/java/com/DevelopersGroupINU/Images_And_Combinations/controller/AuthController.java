4a42de022d162fed04e6819baf22018ee6ee0a21
mehmetakifsalman <ahmetakifsalman@gmail.com>
2024-12-02 15:27:18 +0300

    Auth origin added

--- Images-And-Combinations/src/main/java/com/DevelopersGroupINU/Images_And_Combinations/controller/AuthController.java
+++ Images-And-Combinations/src/main/java/com/DevelopersGroupINU/Images_And_Combinations/controller/AuthController.java
@@ -6,14 +6,12 @@ import com.DevelopersGroupINU.Images_And_Combinations.dto.authDtos.RegisterDto;
 import com.DevelopersGroupINU.Images_And_Combinations.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 public class AuthController {
 
     private final AuthService authService;
